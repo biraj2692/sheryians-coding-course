65f45cb0fa5b4b3745757c8ee0fd203344a26223
biraj2692 <birajdesai9@gmail.com>
2025-04-11 13:01:42 +0530

    intro-to-js

---
+++ JAVASCRIPT/script.js
@@ -0,0 +1,31 @@
+console.log('Hello');
+
+console.warn('This is Warning');
+
+console.error('This is Error');
+
+var a = 10;
+var b = 20;
+
+console.log(a*b);
+console.log(a-b);
+console.log(a+b);
+console.log(a/b);
+console.log(a%b);
+
+// alert('This is Alert');
+// confirm('Are you sure?')
+// var age  = prompt('Enter your age')
+// console.log(age);
+
+
+console.log(a++); // 10
+console.log(++a); // 12
+console.log(a--); // 12
+console.log(--a); // 10
+
+var v = 10; //global scope you can check in windows
+
+const c = 10;
+
+let l = 10; // local scope
