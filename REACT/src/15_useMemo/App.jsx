7b0ed3ff3fd0744ba186696d7922c2e360720a58
biraj2692 <birajdesai9@gmail.com>
2025-06-24 21:39:11 +0530

    useeRef and styled-component

--- REACT/src/15_useMemo/App.jsx
+++ REACT/src/15_useMemo/App.jsx
@@ -1,17 +1,42 @@
-import { useCallback, useMemo, useState } from "react";
+import { useCallback, useMemo, useRef, useState } from "react";
+import styled from "styled-components";
 import Recipe from "./Recipe";
 
 const App = () => {
+  const inputref = useRef();
   const [add, setAdd] = useState(0);
   const [sub, setSub] = useState(99);
+
+  // const Title = styled.h1`
+  //   font-size: 3rem;
+  //   text-align: center;
+  //   color: #bf4f74;
+  //   background-color: ${(props) => props.bg}
+  // `;
+
+  const bg = "pink";
+
+  const Title = styled.h1({
+    fontSize: "3rem",
+    textAlign: "center",
+    color: "#bf4f74",
+    backgroundColor: bg,
+    marginBottom: "10px",
+  });
+  
   // const product = useMemo(() => {
   //   return add * 2;
   // }, [add]);
 
   const ingredients = useCallback(() => {
     console.log("Reciepe Ingredients");
-    
-  },[])
+  }, []);
+
+  const watchHandler = () => {
+    console.log(inputref.current.value);
+    inputref.current.style.color = "blue";
+  };
+
   return (
     <div className="bg-gray-800 text-white p-5 w-screen h-screen">
       {/* <h1 className="text-5xl m-5 p-5 ">{product}</h1> */}
@@ -33,7 +58,24 @@ const App = () => {
       </div>
 
       <br />
-      <Recipe ing = {ingredients}/>
+      <Recipe ing={ingredients} />
+
+      <hr className="my-5" />
+      <Title bg="pink">Let's Learn useRef</Title>
+      <input
+        type="text"
+        ref={inputref}
+        className="outline-0 text-black border-b p-2 text-5xl"
+        placeholder="Enter here"
+      />
+
+      <button
+        className="p-2 text-5xl border mx-2"
+        onClick={watchHandler}
+        type="button"
+      >
+        See Value
+      </button>
     </div>
   );
 };
